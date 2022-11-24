649696236ffd3c248cf827c4a8774419d24c11e9
GordonOtieno <otienogordon95@gmail.com>
2022-11-24 13:29:39 -0800

    Add user and post validators

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,12 @@ class Post < ApplicationRecord
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def update_post_counter
-    author.increment!(:postsCounter) 
+    author.increment!(:postsCounter)
   end
 
   def most_recent_comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :postsCounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def most_recent_posts
     posts.order(created_at: :desc).includes(:author).limit(5)
   end
