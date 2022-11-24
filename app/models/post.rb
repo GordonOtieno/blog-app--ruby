294f63503083ed3e554418040703ebf248764dad
GordonOtieno <otienogordon95@gmail.com>
2022-11-24 12:10:39 -0800

    Solve errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  after_save :update_post_counter
 
   def update_post_counter
-    author.increment!(:posts_counter)
+    author.increment!(:postsCounter) 
   end
 
   def most_recent_comments
