207fa71b03a7aca0ec0a777db53928c56c140957
GordonOtieno <otienogordon95@gmail.com>
2022-12-08 20:18:38 +0300

    Update views and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :postsCounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def most_recent_posts
-    posts.order(created_at: :desc).includes(:author).limit(5)
+    posts.order(created_at: :desc).limit(5)
   end
 end
