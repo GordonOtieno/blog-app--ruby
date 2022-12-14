d6d7cbcd2fa3fbfa8f9878fe747ebc3fadaeee26
GordonOtieno <otienogordon95@gmail.com>
2022-12-14 17:59:09 +0300

    Implement authorization using cancancan

---
+++ app/models/ability.rb
@@ -0,0 +1,6 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user) # rubocop:disable Metrics/MethodLength
+   
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,10 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
   after_save :update_comment_counter
 
+  def update_comment_counter_when_destroy
+    post.decrement!(:comments_counter)
+  end
+
   private
 
   def update_comment_counter
