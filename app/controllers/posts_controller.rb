c2d291a5c9cd15f33e79c3968d670744107784b9
t-bello7 <bello4aus@gmail.com>
2022-12-15 08:20:02 +0100

    - update posts_controller - update ability.rb file from models - update _modal.html.erb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,20 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
-
+​
   def show
-    @post = Post.includes(:author, :comments, :likes).find(params[:id])
+    @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
-    @comments = @post.comments
+    @comments = @post.comments.includes(:author)
   end
-
+​
   def new
     @post = Post.new
   end
-
+​
   def create
     @post = Post.new(post_params)
-
+​
     @post.author_id = current_user.id
     if @post.save
       redirect_to user_post_path(current_user, @post)
@@ -24,11 +24,11 @@ class PostsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
+​
   def edit
     @post = Post.find(params[:id])
   end
-
+​
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
@@ -37,15 +37,22 @@ class PostsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-
+​
   def destroy
     @post = Post.find(params[:id])
+    @post.comments.destroy_all
+    @post.likes.destroy_all
     @post.destroy
-    redirect_to user_posts_path(current_user)
+    if @post.destroy
+      @post.update_posts_count_when_destroy
+      redirect_to user_posts_path(current_user.id), notice: 'Post deleted successfully'
+    else
+      render :new, status: :unprocessable_entity, alert: 'Something'
+    end
   end
-
+​
   private
-
+​
   def post_params
     params.require(:post).permit(:title, :text)
   end
